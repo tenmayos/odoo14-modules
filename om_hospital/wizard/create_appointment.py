ded1b152ebbbe91875f0ea7f1bb9c0cb34a3cebc
bassam <tenmayos_2@hotmail.com>
2023-04-27 09:00:36 -0400

    Added a

--- om_hospital/wizard/create_appointment.py
+++ om_hospital/wizard/create_appointment.py
@@ -1,4 +1,8 @@
+import base64
+from io import StringIO
+from werkzeug.wrappers import Response
 from odoo import api, fields, models
+from odoo import http
 
 
 class CreateAppointmentWizard(models.TransientModel):
@@ -6,3 +10,17 @@ class CreateAppointmentWizard(models.TransientModel):
     _description = "This wizard eases appointment creation"
 
     name = fields.Char(string='Name')
+
+    def ConfirmButton(self):
+        patients = self.env['hospital.patient'].search([])
+        print(self.name)
+        with StringIO() as textBuffer:
+            for obj in patients:
+                textBuffer.write(str(obj.age) + '\n')
+
+            textBuffer.seek(0)
+
+            with open('patients.txt', 'w') as file:
+                file.write(textBuffer.getvalue())
+
+        # TODO: create a controller to handle the download
