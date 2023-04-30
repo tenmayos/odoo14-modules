fe48de631fbf46d0df65ce023210ab33ebc4609f
bassam <tenmayos_2@hotmail.com>
2023-04-30 08:54:23 -0400

    finally got the controller to work and send over file data. this was quiet the journey

--- om_hospital/controllers/hospital.py
+++ om_hospital/controllers/hospital.py
@@ -1,7 +1,29 @@
+from io import StringIO
 from odoo import http
+from odoo.http import request
+from werkzeug import Response
 
 
 class HospitalController(http.Controller):
     @http.route('/hello', auth='public')
     def Hello(self, name):
-        print("I AM HEEEEEEEEEEEEERE!!" + " " + name)
+        patients = request.env['hospital.patient'].search([])
+        for obj in patients:
+            print(obj.age)
+
+        with StringIO() as textBuffer:
+            for obj in patients:
+                textBuffer.write(str(obj.age) + '\n')
+
+            textBuffer.seek(0)
+
+            response = Response(
+                textBuffer.getvalue(),
+                headers={
+                    'Content-Type': 'application/octet-stream',
+                    'Content-Disposition': 'attachment; filename=%s' % name + '.txt',
+                },
+            )
+            return response
+
+
