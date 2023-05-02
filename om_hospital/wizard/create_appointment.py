cba21d2e7c979cd9e8ba07ae63d855e685536ec1
bassam <tenmayos_2@hotmail.com>
2023-05-02 02:11:37 -0400

    modified  to comply with  changes and Removed unused imports

--- om_hospital/wizard/create_appointment.py
+++ om_hospital/wizard/create_appointment.py
@@ -1,8 +1,4 @@
-import base64
-from io import StringIO
-from werkzeug.wrappers import Response
 from odoo import api, fields, models
-
 
 
 class CreateAppointmentWizard(models.TransientModel):
@@ -14,18 +10,6 @@ class CreateAppointmentWizard(models.TransientModel):
     def ConfirmButton(self):
         return {
             'type': 'ir.actions.act_url',
-            'url': '/hello?name=%s' % self.name,
+            'url': '/download/patients_age_text?name=%s' % self.name,
             'target': 'self'
         }
-        # patients = self.env['hospital.patient'].search([])
-        # print(self.name)
-        # with StringIO() as textBuffer:
-        # for obj in patients:
-        #   textBuffer.write(str(obj.age) + '\n')
-
-        # textBuffer.seek(0)
-
-        # with open('patients.txt', 'w') as file:
-        #   file.write(textBuffer.getvalue())
-
-        # TODO: create a controller to handle the download
