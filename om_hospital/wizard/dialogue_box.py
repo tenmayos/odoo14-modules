9a373df3e8fdd52465bd50696b66421c47b94366
bassam <tenmayos_2@hotmail.com>
2023-05-14 06:11:16 -0400

    Added a new TransientModel (wizard) and added it to the initializer

---
+++ om_hospital/wizard/dialogue_box.py
@@ -0,0 +1,10 @@
+from odoo import api, models, fields
+
+
+class DialogueBox(models.TransientModel):
+
+    _name = 'dialogue.box.wizard'
+    _description = 'Creates a dialogue box with costume messages'
+
+    prompt_msg = fields.Char(string='Message')
+
